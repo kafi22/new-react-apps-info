253375836aa7172528d6eb7182d3dc04d17312ba
Your Name <you@example.com>
2023-08-26 23:44:08 +0600

    fix-issue

---
+++ src/Database/Data.js
@@ -0,0 +1,45 @@
+
+import img1 from "../assets/image/my-first-react-apps.netlify.app_ copy.png";
+import img2 from "../assets/image/unplash-clone-apps.netlify.app_.png";
+import img3 from "../assets/image/react-hook-form-with-material-ui-site.netlify.app_ (2) copy.png";
+import img4 from "../assets/image/color-generators-app.netlify.app_ copy.png";
+import img5 from "../assets/image/contentful-app-me.netlify.app_ copy.png";
+
+
+const Data = [
+
+    {
+        title : "my-first-react-apps",
+        url : "https://my-first-react-apps.netlify.app",
+        image : img1,
+        offer : ""
+    }, 
+
+    {
+        title : "unplash-clone-apps",
+        url : "https://unplash-clone-apps.netlify.app",
+        image : img2,
+        offer : ""
+    }, 
+
+    {
+        title : "react form hook with mui",
+        url : "https://react-hook-form-with-material-ui-site.netlify.app",
+        image : img3,
+        offer : ""
+    }, 
+
+    {
+        title : "color-generators-app",
+        url : "https://color-generators-app.netlify.app",
+        image : img4,
+        offer : ""
+    }, 
+
+    {
+        title : "contentful-app-me",
+        url : "https://contentful-app-me.netlify.app",
+        image : img5,
+        offer : ""
+    }, 
+]
