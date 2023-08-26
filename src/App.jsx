f44bb91ebb7c3a9b65a5e6205db60a59028692a6
Your Name <you@example.com>
2023-08-26 18:12:01 +0600

    some-feat-add

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+import {Box} from "@mui/material"
+import Hero from "./Component/Hero";
+import Projects from "./Component/Projects";
+
+
+const Apps = () => {
+
+  return (
+    <Box component={"div"} >
+
+      <Hero />
+      <Projects />
+    </Box>
+  )
+}
+
+export default Apps
